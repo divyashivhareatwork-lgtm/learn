afb1fd923faa5494a57065dab63962e2970dbe72
Adya Pathak <adya.pathak@lognormal.io>
2025-12-19 20:29:52 +0530

    Changes_v2

--- git-training/03-merge-conflicts/app.js
+++ git-training/03-merge-conflicts/app.js
@@ -1,5 +1,1 @@
-function greet(){ return "Hello Adya!"; }
-
-function greet(){ return "Hello World! From Ritika"; }
-
-function greet(){ return "Hello People!"; }
+function greet(){ return "Hello lognormal!"; }
