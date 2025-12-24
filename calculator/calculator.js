d546dbc350fb9830241e705f0b1f68f0114534e4
ankit_jaiswal <aj.mnnit14@gmail.com>
2025-12-24 17:15:16 +0530

    added power method test

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -14,7 +14,7 @@ function div(a, b) {
 function mod(a, b) {
   return Number(a) % Number(b);
 }
-function pow(a, b) {
-  return Number(a) ** Number(b);
+function pow(a, c) {
+  return Number(a) ** Number(c);
 }
 module.exports = { add, sub, mul, div, mod, pow };
